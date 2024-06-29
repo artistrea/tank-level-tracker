fc2ac634e1b7220e5a4098799c574be002a9bff5
artistrea <apiravesi@gmail.com>
2024-06-29 18:10:12 -0300

    update database to let seeding

--- BD/app/controllers.py
+++ BD/app/controllers.py
@@ -1,8 +1,13 @@
 from .exceptions import HTTPException, UnprocessableEntityException, InternalServerErrorException
-from flask import Blueprint, request, jsonify
-from .models import query_db, execute_db
-from .services import auth_service
+from flask import Blueprint, request, jsonify, make_response
+from .models import DB
+from .services.auth_service import AuthService
 from schema import Schema, And, Use, SchemaError
+from flask import g
+
+db = DB(g)
+
+auth_service = AuthService(db)
 
 bp = Blueprint('main', __name__)
 
@@ -19,9 +24,6 @@ def internal_server_error_handler(error: Exception):
     return InternalServerErrorException().get_response()
 
 bp.register_error_handler(Exception, internal_server_error_handler)
-
-bp = Blueprint('main', __name__)
-
 
 def check(conf_schema, conf):
     try:
@@ -68,8 +70,8 @@ class TanksController:
 
     @bp.route("/tanks", methods = ["GET"])
     def getAll():
-        auth_service.authorize_request(request, "read", "tank")
-        tanks = query_db("SELECT * FROM tanks")
+        # auth_service.authorize_request(request, "read", "tank")
+        tanks = db.query_db("SELECT * FROM tanks")
 
         return jsonify([dict(row) for row in tanks]), 200
 
@@ -78,7 +80,7 @@ class TanksController:
     @bp.route("/tanks/<int:id>", methods = ["GET"])
     def getById(id):
         auth_service.authorize_request(request, "read", "tank")
-        tank = query_db("SELECT * FROM tanks WHERE id = ?", [id], one=True)
+        tank = db.query_db("SELECT * FROM tanks WHERE id = ?", [id], one=True)
 
         if tank:
             return jsonify(dict(tank)), 200
@@ -95,8 +97,8 @@ class TanksController:
         if not check(create_tank_schema, data):
             return jsonify({"message": "It's not possible to create a tank without 'top_to_liquid_distance_in_cm', 'tank_base_area', or 'volume'!"}), 400
 
-        tank_id = execute_db(f"INSERT INTO tanks ({", ".join(tanks_parameters)}) VALUES ({", ".join(8*["?"])})", [data.get(var) for var in tanks_parameters]) 
-        created_tank = query_db("SELECT * FROM tanks WHERE id = ?", [tank_id], one=True)
+        tank_id = db.execute_db(f"INSERT INTO tanks ({', '.join(tanks_parameters)}) VALUES ({', '.join(8*['?'])})", [data.get(var) for var in tanks_parameters]) 
+        created_tank = db.query_db("SELECT * FROM tanks WHERE id = ?", [tank_id], one=True)
 
         return jsonify(dict(created_tank)), 201
 
@@ -110,13 +112,14 @@ class TanksController:
         if not check(create_tank_schema, data):
             return jsonify({"message": "It's not possible to update the tank without 'top_to_liquid_distance_in_cm', 'tank_base_area', or 'volume'!"}), 400
 
-        tank = query_db("SELECT * FROM tanks WHERE id = ?", [id], one=True)
+        tank = db.query_db("SELECT * FROM tanks WHERE id = ?", [id], one=True)
 
         if not tank:
             return jsonify({"error": "Tank not found!"}), 404
 
-        execute_db(f"UPDATE tanks SET {','.join([f"{var} = ?" for var in tanks_parameters])} WHERE id = ?", [data.get(var) for var in tanks_parameters]+[id])
-        updated_tank = query_db("SELECT * FROM tanks WHERE id = ?", [id], one=True)
+        question_marks = ','.join([f"{var} = ?" for var in tanks_parameters])
+        db.execute_db(f"UPDATE tanks SET {question_marks} WHERE id = ?", [data.get(var) for var in tanks_parameters]+[id])
+        updated_tank = db.query_db("SELECT * FROM tanks WHERE id = ?", [id], one=True)
 
         return jsonify(dict(updated_tank)), 200
     
@@ -125,16 +128,14 @@ class TanksController:
     @bp.route("/tanks/<int:id>", methods = ["DELETE"])
     def delete_tank(id):
         auth_service.authorize_request(request, "delete", "tank")
-        tank = query_db("SELECT * FROM tanks WHERE id = ?", [id], one=True)
+        tank = db.query_db("SELECT * FROM tanks WHERE id = ?", [id], one=True)
 
         if not tank:
             return jsonify({"error": "Tank not found!"}), 404
         
-        execute_db("DELETE FROM tanks WHERE id = ?", [id])
+        db.execute_db("DELETE FROM tanks WHERE id = ?", [id])
 
         return jsonify({"message": "Tank data deleted successfully!"}), 200
-
-
 
 
 
@@ -147,8 +148,8 @@ class SamplesController:
         if not check(create_samples_schema, data):
             return jsonify({"message": "Invalid Data!"}), 400
 
-        sample_id = execute_db(f"INSERT INTO samples ({", ".join(samples_parameters)}) VALUES ({", ".join(2*["?"])})", [data.get(var) for var in samples_parameters])
-        created_sample = query_db("SELECT * FROM samples WHERE id = ?", [sample_id], one=True)
+        sample_id = db.execute_db(f"INSERT INTO samples ({', '.join(samples_parameters)}) VALUES ({', '.join(2*['?'])})", [data.get(var) for var in samples_parameters])
+        created_sample = db.query_db("SELECT * FROM samples WHERE id = ?", [sample_id], one=True)
 
         return jsonify(dict(created_sample)), 201
 
@@ -157,12 +158,12 @@ class SamplesController:
     @bp.route("/samples/<int:id>", methods = ["DELETE"])
     def delete_sample(id):
         auth_service.authorize_request(request, "delete", "sample")
-        sample = query_db("SELECT * FROM samples WHERE id = ?", [id], one=True)
+        sample = db.query_db("SELECT * FROM samples WHERE id = ?", [id], one=True)
 
         if not sample:
             return jsonify({"error": "Sample not found!"}), 404
         
-        execute_db("DELETE FROM samples WHERE id = ?", [id])
+        db.execute_db("DELETE FROM samples WHERE id = ?", [id])
 
         return jsonify({"message": "Sample data deleted successfully!"}), 200
 
---
+++ BD/app/seed.py
@@ -0,0 +1,99 @@
+# python3 ./src/seed.py
+
+import sys
+import os
+APP_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.dirname(APP_DIR))
+
+from app.services.auth_service import AuthService
+from app.models import DB, init_db
+
+db = DB()
+auth_service = AuthService(db)
+
+
+tanks_parameters = [
+    "name",
+    "description",
+    "maximum_volume",
+    "volume_danger_zone",
+    "volume_alert_zone",
+    "tank_base_area",
+    "latitude",
+    "longitude"
+]
+
+samples_parameters = [ 
+    "tank_id",
+    "top_to_liquid_distance_in_cm"
+]
+
+def seed():
+    auth_service.create_user("admin@gmail.com", "123456", "O Professor")
+    auth_service.create_user("aluno@gmail.com", "123456", "O Aluno")
+
+    new_tank = {
+        "name": "Tanque 1",
+        "description": "Tanque 1",
+        "maximum_volume": 10.5,
+        "volume_danger_zone": 2,
+        "volume_alert_zone": 5,
+        "tank_base_area": 2,
+        "latitude": -47.862,
+        "longitude": -15.7657
+    }
+    tank_id = db.execute_db(f"INSERT INTO tanks ({', '.join(tanks_parameters)}) VALUES ({', '.join(8*['?'])})", [new_tank.get(var) for var in tanks_parameters]) 
+    new_samples = [
+        {
+            "tank_id": tank_id,
+            "top_to_liquid_distance_in_cm": 500
+        },
+        {
+            "tank_id": tank_id,
+            "top_to_liquid_distance_in_cm": 500
+        },
+        {
+            "tank_id": tank_id,
+            "top_to_liquid_distance_in_cm": 400
+        },
+        {
+            "tank_id": tank_id,
+            "top_to_liquid_distance_in_cm": 300
+        },
+        {
+            "tank_id": tank_id,
+            "top_to_liquid_distance_in_cm": 200
+        },
+        {
+            "tank_id": tank_id,
+            "top_to_liquid_distance_in_cm": 190
+        },
+    ]
+
+    for new_sample in new_samples:
+        db.execute_db(f"INSERT INTO samples ({', '.join(samples_parameters)}) VALUES ({', '.join(2*['?'])})", [new_sample.get(var) for var in samples_parameters])
+
+    new_tank = {
+        "name": "Tanque 2",
+        "description": "Tanque 2",
+        "maximum_volume": 10.5,
+        "volume_danger_zone": 2,
+        "volume_alert_zone": 5,
+        "tank_base_area": 2,
+        "latitude": -47.868,
+        "longitude": 15.765
+    }
+    tank_id = db.execute_db(f"INSERT INTO tanks ({', '.join(tanks_parameters)}) VALUES ({', '.join(8*['?'])})", [new_tank.get(var) for var in tanks_parameters]) 
+    new_sample = {
+        "tank_id": tank_id,
+        "top_to_liquid_distance_in_cm": 200
+    }
+
+    db.execute_db(f"INSERT INTO samples ({', '.join(samples_parameters)}) VALUES ({', '.join(2*['?'])})", [new_sample.get(var) for var in samples_parameters])
+
+    print("seed executed!")
+
+
+if __name__ == "__main__":
+    init_db()
+    seed()
