25d4930f419f8a8b647624dda4bd2ca32260459e
LaKHamote <lucascboa@gmail.com>
2024-06-30 22:36:34 -0300

    ⚡️

--- web/src/pages/map.tsx
+++ web/src/pages/map.tsx
@@ -68,14 +68,14 @@ export default function MapPage() {
   const [maximumVolume, setTankMaximumVolume] = useState(0);
   const [dangerZone, setTankDangerZone] = useState(0);
   const [alertZone, setTankAlertZone] = useState(0);
-  const [volume, setTankVolume] = useState(0);
+  const [tankBaseArea, setTankBaseArea] = useState(0);
 
   const handleCreate = (e:any) => {
     if(!name &&
       !maximumVolume &&
       !dangerZone &&
       !alertZone &&
-      !volume
+      !tankBaseArea
       ){
       alert("Preencha todos os campos")
     }else if(
@@ -84,29 +84,20 @@ export default function MapPage() {
       ){
       alert("Selecione uma localização no mapa")  
     }else{
-      console.log({
-        name,
-        maximumVolume,
-        dangerZone,
-        alertZone,
-        volume,
-        lat,
-        long
-      })
       baseApi.post("/tanks", {
         name: name,
         description: name,
         maximum_volume: maximumVolume,
         volume_danger_zone: dangerZone,
         volume_alert_zone: alertZone,
-        tank_base_area: 200, //TODO: como criar isso e se volume atual faz sentido perguntar
+        tank_base_area: tankBaseArea, //TODO: como criar isso e se volume atual faz sentido perguntar
         latitude: long, //TODO: long e lat foram invertidos, no codigo todo, inclusive funcoes to/fromLongLat 
         longitude: lat
       })
       .then((res)=> {
         baseApi.post("/samples", {
           tank_id: res.data.id,
-          top_to_liquid_distance_in_cm: 200
+          top_to_liquid_distance_in_cm: 0 //OBS: o tanque adicionado deve estar cheio
         })
         console.log(res)
         location.reload()
@@ -191,7 +182,7 @@ export default function MapPage() {
                         </div>
                         <div className="m-1">
                             <label className="m-1">
-                                Volume Máximo:
+                                Volume Máximo(litros):
                                 <input
                                     className="bg-zinc-800 border-b m-1"
                                     type="number"
@@ -204,7 +195,7 @@ export default function MapPage() {
                         </div>
                         <div className="m-1">
                             <label className="m-1">
-                                Zona de alerta:
+                                Zona de alerta(litros):
                                 <input
                                     className="bg-zinc-800 border-b m-1"
                                     type="number"
@@ -218,7 +209,7 @@ export default function MapPage() {
                         </div>
                         <div className="m-1">
                             <label className="m-1">
-                                Zona de Perigo:
+                                Zona de Perigo(litros):
                                 <input
                                     className="bg-zinc-800 border-b m-1"
                                     type="number"
@@ -232,15 +223,14 @@ export default function MapPage() {
                         </div>
                         <div className="m-1">
                             <label className="m-1">
-                                Volume Atual:
+                                Área da Base do tanque(m²):
                                 <input
                                     className="bg-zinc-800 border-b m-1"
                                     type="number"
                                     min="0"
-                                    max={maximumVolume}
-                                    name="volumeAtual"
-                                    value={volume}
-                                    onChange={(e)=>setTankVolume(parseInt(e.target.value))}
+                                    name="tank_base_area"
+                                    value={tankBaseArea}
+                                    onChange={(e)=>setTankBaseArea(parseInt(e.target.value))}
                                 />
                             </label>
                         </div>
